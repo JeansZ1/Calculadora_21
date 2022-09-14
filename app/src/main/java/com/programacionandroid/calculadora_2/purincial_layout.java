fbdf7ab988acdbd59ebfd3fe433d24a2026d0518
JeansZ <jeanszepeda@gmail.com>
2022-09-14 17:26:33 -0300

    version 1.2

---
+++ app/src/main/java/com/programacionandroid/calculadora_2/purincial_layout.java
@@ -0,0 +1,15 @@
+package com.programacionandroid.calculadora_2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class purincial_layout  extends AppCompatActivity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.princial_layout);
+
+   }
+
+}
